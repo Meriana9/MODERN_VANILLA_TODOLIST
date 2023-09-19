59c75ab337786ef8d310702f089f7946fb576020
Meriana <merianaalchami@gmail.com>
2023-09-19 11:46:57 +0200

    mise en place des composants

---
+++ js/DB.js
@@ -0,0 +1,10 @@
+export default class DB {
+  static setApiURL(data) {
+    this.apiURL = data;
+  }
+
+  static async findAll() {
+    const reponse = await fetch(this.apiURL + "/todos");
+    return await reponse.json();
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-import '../style.scss'
+import "../style.scss";
+import TodoList from "./components/todoList/TodoList";
+
+//Instancier une nouvelle todolist
+//en lui envoyant l'élément DOM sur lequel se greffer
+// e l'URL de l'API à utiliser : https://6501f8dc736d26322f5c93c6.mockapi.io/
+
+new TodoList({
+  apiURL: "https://6501f8dc736d26322f5c93c6.mockapi.io/",
+  domELT: "#app",
+});
